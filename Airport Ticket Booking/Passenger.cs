ba40403918bc43990b03c6ba6cb398de5db08926
Heba Ashour <hashour2098@gmail.com>
2023-07-31 23:23:59 +0300

    Make Skeleton for Passenger Main

---
+++ Airport Ticket Booking/Passenger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking
+{
+    enum PassengerOptions
+    {
+        Book = 1, Show = 2, Modify = 3, Cancel = 4, View = 5, Exit = 6
+    }
+    public class Passenger
+    {
+        public void PassengerMain()
+        {
+            var flag = true;
+            while (flag)
+            {
+                Console.WriteLine("***************************************************");
+                Console.WriteLine("************** 1. Book a Flight *******************");
+                Console.WriteLine("************** 2. Show Avaliable Flights **********");
+                Console.WriteLine("************** 3. Modify Booking ******************");
+                Console.WriteLine("************** 4. Cancel Booking ******************");
+                Console.WriteLine("************** 5. View Bookings *******************");
+                Console.WriteLine("************** 6. Exit ****************************");
+                Console.WriteLine("***************************************************");
+
+                string userInput = Console.ReadLine();
+                if (!int.TryParse(userInput, out int number))
+                {
+                    Console.WriteLine("Please Choose Number from 1 to 6");
+                    continue;
+                }
+
+                switch (number)
+                {
+                    case (int) PassengerOptions.Book:
+                        break;
+                    case (int) PassengerOptions.Show:
+                        break;
+                    case (int) PassengerOptions.Modify:
+                        break;
+                    case (int) PassengerOptions.Cancel:
+                        break;
+                    case (int) PassengerOptions.View:
+                        break;
+                    case (int) PassengerOptions.Exit:
+                        flag = false;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+    }
+}
--- Airport Ticket Booking/Program.cs
+++ Airport Ticket Booking/Program.cs
@@ -10,10 +10,11 @@ namespace Airport_Ticket_Booking
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("**************************************************");
             Console.WriteLine("****** Welcome to Airport Ticket Booking *********");
             Console.WriteLine("****** If you are Passenger please press 1 *******");
             Console.WriteLine("****** If your are Manager please press 2 ********");
+            Console.WriteLine("**************************************************");
 
             string userInput = Console.ReadLine();
             int number;
@@ -22,10 +23,12 @@ namespace Airport_Ticket_Booking
                 Console.WriteLine("Please Choose Either 1 or 2");
                 userInput = Console.ReadLine();
             }
-            
 
+            var flag = true;
             if(number == 1)
             {
+                Passenger pass = new Passenger();
+                pass.PassengerMain();
 
             }
             else
@@ -34,7 +37,6 @@ namespace Airport_Ticket_Booking
             }
 
             //Common.ReadFromCSV("C:\\Users\\Heba Ashour\\source\\repos\\Airport Ticket Booking\\flights.csv");
-            Console.ReadKey();
         }
     }
 }
