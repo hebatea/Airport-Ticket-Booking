9f4a3da86ec46c3203cf804668bc61be1159f44c
Heba Ashour <hashour2098@gmail.com>
2023-08-03 22:00:16 +0300

    Implement "GetFieldConstraints"

--- Airport Ticket Booking/Flight.cs
+++ Airport Ticket Booking/Flight.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using static Airport_Ticket_Booking.Common;
+using static Airport_Ticket_Booking.Manager;
 
 namespace Airport_Ticket_Booking
 {
@@ -20,13 +21,29 @@ namespace Airport_Ticket_Booking
         {
             get => id;
         }
+
+        [FlightFieldConstraint("Free Text", true)]
         public string Code { get; set; }
+
+        [FlightFieldConstraint("Double", true, "0 -> infinity")]
         public double Price { get; set; }
+
+        [FlightFieldConstraint("Free Text", true)]
         public string DepartureCountry { get; set; }
+
+        [FlightFieldConstraint("Free Text", true)]
         public string DestinationCountry { get; set; }
+
+        [FlightFieldConstraint("Date Time", true, "today -> future")]
         public DateTime DepartureDate { get; set; }
+
+        [FlightFieldConstraint("Free Text", true)]
         public string DepartureAirport { get; set; }
+
+        [FlightFieldConstraint("Free Text", true)]
         public string ArrivalAirport { get; set; }
+
+        [FlightFieldConstraint("Number from 1 - 3", true, "1 -> Economy, 2 -> Business, 3 -> First Class")]
         public FlightClass FClass { get; set; }
 
         public override string ToString()
--- Airport Ticket Booking/Manager.cs
+++ Airport Ticket Booking/Manager.cs
@@ -1,16 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Airport_Ticket_Booking.Common;
 
 namespace Airport_Ticket_Booking
 {
     public class Manager
     {
+        FlightsManager FlightsManager;
+        public Manager()
+        {
+            FlightsManager = new FlightsManager();
+        }
+
+        enum ManagerOptions
+        {
+            ShowBookings = 1, AddCSV = 2, FieldsDetails = 3, Exit = 4
+        }
+
         public void ManagerMain()
         {
+            var Flag = true;
+            while(Flag) { 
+                Console.WriteLine("***************************************************");
+                Console.WriteLine("************** 1. Filter Booking ******************");
+                Console.WriteLine("************** 2. Add Flights *********************");
+                Console.WriteLine("************** 3. Flieds Details *********************");
+                Console.WriteLine("************** 4. Exit ****************************");
+                Console.WriteLine("***************************************************");
 
+                int number = (int) UserInput((int)IntOrDouble.integern, 1, 4);
+                switch (number)
+                {
+                    case (int) ManagerOptions.ShowBookings:
+                        break;
+
+                    case (int) ManagerOptions.AddCSV:
+                        
+                        break;
+
+                    case (int)ManagerOptions.FieldsDetails:
+                        GetFieldConstraints();
+                        break;
+                    case (int) ManagerOptions.Exit:
+                        Flag = false;
+                        break;
+                }
+            }
+        }
+
+
+        public static void GetFieldConstraints()
+        {
+            var flightType = typeof(Flight);
+            var properties = flightType.GetProperties();
+            
+            foreach(var property in properties)
+            {
+                var constraints = property.GetCustomAttributes(typeof(FlightFieldConstraintAttribute), true)
+                                         .OfType<FlightFieldConstraintAttribute>()
+                                         .FirstOrDefault();
+
+                if(constraints != null)
+                {
+                    Console.WriteLine($"{property.Name}");
+                    Console.WriteLine($"Data Type: {constraints.DataType}");
+                    Console.WriteLine($"Is Required: {constraints.IsRequired}");
+                    if (!string.IsNullOrEmpty(constraints.AllowedRange)) 
+                        Console.WriteLine($"Allowed Range: {constraints.AllowedRange}");
+                    Console.WriteLine();
+
+                }
+            }
+        }
+
+        [AttributeUsage(AttributeTargets.Property)]
+        public class FlightFieldConstraintAttribute: Attribute
+        {
+            public string DataType { get; set; }
+            public bool IsRequired { get; set; }
+            public string AllowedRange { get; set; }
+
+            public FlightFieldConstraintAttribute(string dataType, bool isRequired, string allowedRange = null)
+            {
+                DataType = dataType;
+                IsRequired = isRequired;
+                AllowedRange = allowedRange;
+            }
         }
     }
 }
