43990819ce161d78747b127f2fa43273be45f4a0
Heba Ashour <hashour2098@gmail.com>
2023-08-03 19:32:59 +0300

    Update the Main Program

---
+++ Airport Ticket Booking/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport_Ticket_Booking
+{
+    public class Manager
+    {
+        public void ManagerMain()
+        {
+
+        }
+    }
+}
--- Airport Ticket Booking/Program.cs
+++ Airport Ticket Booking/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Airport_Ticket_Booking.Common;
 
 namespace Airport_Ticket_Booking
 {
@@ -10,32 +12,36 @@ namespace Airport_Ticket_Booking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("**************************************************");
-            Console.WriteLine("****** Welcome to Airport Ticket Booking *********");
-            Console.WriteLine("******* 1 -> If you are Passenger please *********");
-            Console.WriteLine("******* 2 -> If your are Manager please  *********");
-            Console.WriteLine("**************************************************");
-
-            string userInput = Console.ReadLine();
-            int number;
-            while (!int.TryParse(userInput, out number) || (number != 1 && number != 2))
-            {
-                Console.WriteLine("Please Choose Either 1 or 2");
-                userInput = Console.ReadLine();
-            }
-
             var flag = true;
-            if(number == 1)
-            {
-                Passenger passenger = new Passenger();
-                passenger.PassengerMain();
-
-            }
-            else
+            while (flag)
             {
 
-            }
+                Console.WriteLine("**************************************************");
+                Console.WriteLine("****** Welcome to Airport Ticket Booking *********");
+                Console.WriteLine("******* 1 -> If you are Passenger please *********");
+                Console.WriteLine("******* 2 -> If your are Manager please  *********");
+                Console.WriteLine("****************** 3 -> Exsit  *******************");
+                Console.WriteLine("**************************************************");
 
+
+                int number = (int) UserInput((int)IntOrDouble.integern, 1, 3);
+
+                if (number == 1)
+                {
+                    Passenger passenger = new Passenger();
+                    passenger.PassengerMain();
+
+                }
+                else if (number == 2)
+                {
+                    Manager manager = new Manager();
+                    manager.ManagerMain();
+                }
+                else
+                {
+                    flag = false;
+                }
+            }
             //Common.ReadFromCSV("C:\\Users\\Heba Ashour\\source\\repos\\Airport Ticket Booking\\flights.csv");
         }
     }
