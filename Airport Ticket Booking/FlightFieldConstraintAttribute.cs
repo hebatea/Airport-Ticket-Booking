79b8f527e41807ee9845b08794b64b93b9243100
Heba Ashour <hashour2098@gmail.com>
2023-08-19 23:19:55 +0300

    Put FlightFieldConstraintAttribute class in its own file

---
+++ Airport Ticket Booking/FlightFieldConstraintAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Airport_Ticket_Booking
+{
+    public partial class Manager
+    {
+        [AttributeUsage(AttributeTargets.Property)]
+        public class FlightFieldConstraintAttribute : Attribute
+        {
+            public string DataType { get; set; }
+            public bool IsRequired { get; set; }
+            public string AllowedRange { get; set; }
+
+            public FlightFieldConstraintAttribute(string dataType, bool isRequired, string allowedRange = null)
+            {
+                DataType = dataType;
+                IsRequired = isRequired;
+                AllowedRange = allowedRange;
+            }
+        }
+    }
+}
